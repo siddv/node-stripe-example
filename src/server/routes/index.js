7d622627e34986d5c7cd1f8184d74caaf83b83d6
Siddharth Vadgama <me@siddv.net>
2016-11-27 12:38:41 +0000

    generic commit

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,10 +7,23 @@ var products = [
     productName: 'Soylent',
     productDescription: 'Meal replacement that tastes like pancake batter.',
     productPrice: 10.99
+  },
+  {
+    productName: 'Barnados',
+    productDescription: 'wevs.',
+    productPrice: 5
   }
 ];
 
 router.get('/', function(req, res, next) {
+  var productName = 'Soylent';
+  for (var i = 0; i < products.length; i++) {
+    if(productName === products[i].productName) {
+      return res.render('product', {productInfo: products[i]});
+    } else {
+      return res.send('Product does not exist.');
+    }
+  }});
 
 router.get('/products/:name', function(req, res, next) {
   var productName = req.params.name;
@@ -26,11 +39,13 @@ router.get('/products/:name', function(req, res, next) {
 router.post('/charge', function(req, res,next) {
   var stripeToken = req.body.stripeToken;
   var amount = req.body.price * 100;
+  var charity = req.body.charity;
 
   // ensure amount === actual product amount to avoid fraud
 
   stripe.charges.create({
     card: stripeToken,
+    currency: 'gbp',
     amount: amount
   },
   function(err, charge) {
@@ -38,7 +53,8 @@ router.post('/charge', function(req, res,next) {
       console.log(err);
       res.send('error');
     } else {
-      res.send('success');
+      res.send(charge);
+      console.log('charge', charge)
     }
   });
 });
