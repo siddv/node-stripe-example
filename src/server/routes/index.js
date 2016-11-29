7056af765a9aa4d605ef17f6b021f1cf8e7491ec
Siddharth Vadgama <me@siddv.net>
2016-11-29 14:18:52 +0000

    Allow access control

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -37,6 +37,10 @@ router.get('/products/:name', function(req, res, next) {
 });
 
 router.post('/charge', function(req, res,next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
   var stripeToken = req.body.stripeToken;
   var amount = req.body.price * 100;
   var charity = req.body.charity;
